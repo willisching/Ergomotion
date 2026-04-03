8619f92b43fe047cccc879cdcb6da8d459a28f7a
willisching <87769653+willisching@users.noreply.github.com>
2026-04-03 02:02:19 -0400

    keep select

--- custom_components/ergomotion/__init__.py
+++ custom_components/ergomotion/__init__.py
@@ -5,7 +5,7 @@ from homeassistant.core import HomeAssistant, callback
 from .core import DOMAIN
 from .core.device import Device
 
-PLATFORMS = ["binary_sensor", "light", "button", "switch"]
+PLATFORMS = ["binary_sensor", "light", "select", "button", "switch"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
